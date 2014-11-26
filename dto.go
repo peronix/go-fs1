04829d50d1751dad8459437f97802adc16627a96
Brent Anderson <brentmn@gmail.com>
2014-11-26 17:01:45 -0600

    - added : some data transfer objects

---
+++ dto.go
@@ -0,0 +1,110 @@
+package fs1
+
+type Primary struct {
+	Id  string `json:"@id"`
+	Uri string `json:"@uri"`
+}
+type PrimaryAndName struct {
+	Primary
+	Name string `json:"name"`
+}
+type Audit struct {
+	CreatedDate     string `json:"createdDate"`
+	LastUpdatedDate string `json:"lastUpdatedDate"`
+}
+
+// type Security struct {
+// 	LastSecurityAuthorization string `json:"lastSecurityAuthorization"`
+// 	LastActivityDate          string `json:"lastActivityDate"`
+// }
+
+type Person struct {
+	Person struct {
+		Primary
+		ImageURI            string         `json:"@imageURI"`
+		OldId               string         `json:"@oldID"`
+		ICode               string         `json:"@iCode"`
+		HouseholdId         string         `json:"@householdID"`
+		OldHouseholdId      string         `json:"@oldHouseholdID"`
+		Title               string         `json:"title"`
+		Salutation          string         `json:"salutation"`
+		Prefix              string         `json:"prefix"`
+		FirstName           string         `json:"firstName"`
+		LastName            string         `json:"lastName"`
+		Suffix              string         `json:"suffix"`
+		MiddleName          string         `json:"middleName"`
+		GoesByName          string         `json:"goesByName"`
+		FormerName          string         `json:"formerName"`
+		Gender              string         `json:"genderName"`
+		DateOfBirth         string         `json:"dateOfBirth"`
+		MaritalStatus       string         `json:"maritalStatus"`
+		HouseholdMemberType PrimaryAndName `json:"householdMemberType"`
+		IsAuthorized        string         `json:"isAuthorized"`
+		Status              struct {
+			PrimaryAndName
+			Comment   string         `json:"comment"`
+			Date      string         `json:"date"`
+			SubStatus PrimaryAndName `json;"subStatus"`
+		} `json:"status"`
+		Occupation struct {
+			PrimaryAndName
+			Description string `json:"description"`
+		} `json:"occupation"`
+		Employer           string         `json:"employer"`
+		School             PrimaryAndName `json:"school"`
+		Denomination       PrimaryAndName `json:"denomination"`
+		FormerChurch       string         `json:"formerChurch"`
+		BarCode            string         `json:"barcode"`
+		MemberEnvolopeCode string         `json;"memberEnveolopeCode"`
+		DefaultTagComment  string         `json:"defaultTagComment"`
+		Weblink            struct {
+			UserId         string `json:"userID"`
+			PasswordHint   string `json:"passwordHint"`
+			PasswordAnswer string `json:"passwordAnswer"`
+		} `json:"weblink"`
+		Solicit       string `json:"solicit"`
+		Thank         string `json:"thank"`
+		FirstRecord   string `json:"firstRecord"`
+		LastMatchDate string `json:"lastMatchDate"`
+		Audit
+	} `json:"person"`
+}
+
+type Address struct {
+	Address struct {
+		Primary
+		Household               Primary        `json:"household"`
+		Person                  Primary        `json:"person"`
+		AddressType             PrimaryAndName `json:"addressType"`
+		Address1                string         `json:"address1"`
+		Address2                string         `json:"address2"`
+		Address3                string         `json:"address3"`
+		City                    string         `json:"city"`
+		PostalCode              string         `json:"postalCode"`
+		County                  string         `json:"county"`
+		Country                 string         `json:"country"`
+		StProvince              string         `json:"stProvince"`
+		CarrierRoute            string         `json:"carrierRoute"`
+		DeliveryPoint           string         `json:"deliveryPoint"`
+		AddressDate             string         `json:"addressDate"`
+		AddressComment          string         `json:"addressComment"`
+		USPSVerified            string         `json:"uspsVerified"`
+		AddressVerifiedDate     string         `json:"addressVerifiedDate"`
+		LastVerificationAttempt string         `json:"lastVerificationAttempt"`
+		Audit
+	} `json:"address"`
+}
+type Communication struct {
+	Communication struct {
+		Primary
+		Household                Primary        `json:"household"`
+		Person                   Primary        `json:"person"`
+		CommunicationType        PrimaryAndName `json:"communicationType"`
+		CommunicationTypeGeneral string         `json:"communicationTypeGeneral"`
+		ComminicationValue       string         `json:"communicationValue"`
+		SearchComminicationValue string         `json:"searchCommunicationValue"`
+		Preferred                string         `json:"preferred"`
+		CommunicationComment     string         `json:"communicationComment"`
+		Audit
+	} `json:"communication"`
+}
